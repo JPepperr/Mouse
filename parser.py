1e84576b629dd3d33bda1e53914babea80254f15
JPepperr <shidan3101@yandex.ru>
2022-05-16 00:09:26 +0300

    Create parser, that extracts data

---
+++ parser.py
@@ -0,0 +1,14 @@
+import openpyxl
+
+f = open("raw_data.txt", "wt")
+wookbook = openpyxl.load_workbook("labs.xlsx")
+
+ws = wookbook.active
+
+cnt = 0
+for row in ws.values:
+    if ((row[1] == 'F1-24' and row[3] == 1) or (row[1] == 'I-hpc' and row[3] == 1)):
+        f.write('\t'.join([str(x) for x in row]) + '\n')
+        cnt += 1
+print(cnt)
+f.close()
